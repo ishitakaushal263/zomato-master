a5142c427024e2d46c201e971eff608d5417a7b6
ishitakaushal263 <ishitakaushal263@gmail.com>
2022-01-15 11:18:19 +0530

    change

--- server/API/Auth/index.js
+++ server/API/Auth/index.js
@@ -77,7 +77,7 @@ Router.get(
     passport.authenticate("google", { failureRedirect: "/" }),
     (req, res) => {
         return res.redirect(
-            `http://localhost:3000/google/${req.session.passport.user.token}`
+            `http://localhost:5000/google/${req.session.passport.user.token}`
         );
     }
 );
